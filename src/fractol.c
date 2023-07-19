6feccbe2c88fcb72a191ec1019413aa51abf78d4
agaley <agaley@student.42lyon.fr>
2023-07-19 21:57:33 +0200

    Protect mlx init

--- src/fractol.c
+++ src/fractol.c
@@ -22,7 +22,11 @@ int	main(int argc, char **argv)
 	env.h = 1024;
 	parse_args(argv, &env);
 	env.mlxptr = mlx_init();
+	if (!env.mlxptr)
+		handle_exit(1, &env);
 	env.winptr = mlx_new_window(env.mlxptr, env.w, env.h, "fract-ol");
+	if (!env.winptr)
+		handle_exit(1, &env);
 	mlx_key_hook(env.winptr, handle_keys, &env);
 	mlx_mouse_hook(env.winptr, handle_mouse, &env);
 	mlx_loop(env.mlxptr);
